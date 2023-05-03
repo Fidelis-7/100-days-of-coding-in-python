5fb506e69a3f0a88a6b700163c03308d65bd3c8c
Fidelis-7 <abanumfidelis90@gmail.com>
2023-05-03 00:19:43 -0700

    added day_1

---
+++ 100-Days/Day_1/print_variables.py
@@ -0,0 +1,8 @@
+# Welcome to 100 Days of Coding in python
+first_name = input("enter your first_name: ")
+middle_name = input("enter your middle name: ")
+last_name = input("enter your last name: ")
+age = int(input("enter your age: "))
+print(f"my name is {first_name} {middle_name} {last_name} and I am {age} years old.")
+
+
